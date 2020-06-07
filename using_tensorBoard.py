b99f0aa39702932958e6dd2b9355e4334ca3fbff
youpele52 <43122220+youpele52@users.noreply.github.com>
2020-06-07 08:20:44 +0200

    How to use tensorBoard on DeepMedic

---
+++ using_tensorBoard.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 19:44:40 2020
+
+@author: Youpele
+"""
+
+'''
+To use Tensorboard to view result do the following 
+
+0.Type this in the powershell, don;t forget to change the path
+
+tensorboard --logdir=C:\Users\Youpele\Downloads\tensorboard\trainSessionWithValidTiny\train --host=127.0.0.1
+
+1. Go to your browser and type in this 
+
+localhost:6006
+
+'''
