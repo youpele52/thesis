da46c76cba1d8298f00a92dda311ae1bf0e70d02
youpele52 <43122220+youpele52@users.noreply.github.com>
2020-07-30 12:28:53 +0200

    refactored 27072020

--- resample_spacing.py
+++ resample_spacing.py
@@ -5,7 +5,7 @@ Created on Sat Jul 25 20:46:41 2020
 @author: Orginally written by Liliana, modified by Youpele
 """
 
-# import numpy as np
+import numpy as np
 import SimpleITK as sitk
 
 
@@ -14,18 +14,18 @@ def resample(image_path, out_image_path, segmentation_or_image,
     
     itk_image = sitk.ReadImage(image_path)
     
-    # # Resample images to 2mm spacing with SimpleITK
-    # original_spacing = itk_image.GetSpacing()
-    # original_size = itk_image.GetSize()
+    # Resample images to 2mm spacing with SimpleITK
+    original_spacing = itk_image.GetSpacing()
+    original_size = itk_image.GetSize()
 
-    # out_size = [
-    #     int(np.round(original_size[0] * (original_spacing[0] / out_spacing[0]))),
-    #     int(np.round(original_size[1] * (original_spacing[1] / out_spacing[1]))),
-    #     int(np.round(original_size[2] * (original_spacing[2] / out_spacing[2])))]
+    out_size = [
+        int(np.round(original_size[0] * (original_spacing[0] / out_spacing[0]))),
+        int(np.round(original_size[1] * (original_spacing[1] / out_spacing[1]))),
+        int(np.round(original_size[2] * (original_spacing[2] / out_spacing[2])))]
 
     resample = sitk.ResampleImageFilter()
     resample.SetOutputSpacing(out_spacing)
-    resample.SetSize(itk_image.GetSize())
+    resample.SetSize(out_size)
     resample.SetOutputDirection(itk_image.GetDirection())
     resample.SetOutputOrigin(itk_image.GetOrigin())
     resample.SetTransform(sitk.Transform())
