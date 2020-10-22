763e6382c979fde43fc0c43cc0c906dbb8a77fc2
youpele52 <43122220+youpele52@users.noreply.github.com>
2020-10-22 23:48:55 +0200

    Simple code to get the time it takes to run a code

---
+++ timecapsule.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 23:39:00 2020
+
+@author: youpele
+"""
+
+import time
+
+start = time.time()
+print("hello")
+end = time.time()
+duration = end - start
+ 
+print(duration)
