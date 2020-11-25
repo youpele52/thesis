9f860043825a582922576f40b191884646c89f87
youpele52 <43122220+youpele52@users.noreply.github.com>
2020-11-25 17:16:30 +0100

    Create Normalization.py

---
+++ Normalization.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 15:22:50 2020
+
+@author: michaely
+"""
+
+
+
+import os
+import numpy as np
+import nibabel as nib
+# from shutil import copyfile
+import matplotlib.pyplot as plt
+from scipy import stats
+join = os.path.join
+
+
+def normalization (image_path, export_dir, export_name, create_mask = True, threshold = -900):
+    
+    print("Processing:", image_path)
+    img = nib.load(image_path)
+    I=img.get_fdata()
+    #Define  mask
+    mask=(I>threshold) 
+    plt.imshow(mask[:,:,100])
+    plt.show()
+    plt.imshow(I[:,:,100])
+    plt.show()
+    
+    # normalizing the image
+    tmeanI=stats.tmean(I, (threshold,np.max(I)))
+    tstdI=stats.tstd(np.ndarray.flatten(I), (threshold,np.max(I)))
+    img_new=(I-tmeanI)/tstdI
+    # img_new=(I-101)/76.9
+    plt.imshow(img_new[:,:,100])
+    plt.show()
+    new_img = nib.Nifti1Image(img_new, img.affine, img.header)
+    nib.save(new_img, join(export_dir, export_name + ".nii"))
+    print("Normalized image created.")
+    
+    # creating image mask
+    if create_mask == True:
+        subject_mask = export_name + "_mask"
+        img_mask = nib.Nifti1Image(mask, img.affine, img.header)
+        nib.save(img_mask, join(export_dir, subject_mask + ".nii"))
+        print("Image mask created.")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# normalization(image_path = r"C:\Users\michaely\Documents\hiwi\DeepMedic Stuff\Convert to nifti 20052020\DICOM_Niere\DICOM\P001\windowed_image.nii", 
+#               export_dir = r"C:\Users\michaely\Documents\hiwi\DeepMedic Stuff\Convert to nifti 20052020\DICOM_Niere\DICOM\P001", 
+#               export_name = "wind_norm", threshold = -900)    
+    
+# a =  r"C:\Users\michaely\Documents\hiwi\DeepMedic Stuff\Convert to nifti 20052020\DICOM_Niere\DICOM\P001\resampled_image.nii.gz"
+# b = os.path.basename(a)
+# c = os.path.splitext(b)[0]
