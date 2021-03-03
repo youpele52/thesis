8c989d16a252abd08fedd13e79582ebda8ecc79e
youpele52 <43122220+youpele52@users.noreply.github.com>
2021-03-03 15:06:14 +0100

    Add files via upload

--- CreateCFGs_Prostate.py
+++ CreateCFGs_Prostate.py
@@ -7,7 +7,7 @@ Created on Wed Mar  3 13:45:24 2021
 """
 
 
-from datetime import datetime
+# from datetime import datetime
 import os
 join = os.path.join
 
@@ -17,7 +17,7 @@ join = os.path.join
 
 def create_prostate_cfgs(main_dir, export_dir, search_term  ):
 
-    todays_date = datetime.now().strftime('%d%m20%y')
+    # todays_date = datetime.now().strftime('%d%m20%y')
     dir_name = main_dir.split('/')[-1]
     
     GtLabels = []
@@ -48,22 +48,22 @@ def create_prostate_cfgs(main_dir, export_dir, search_term  ):
                         Channels_adc.append(  join(patient_dir, content) )
                         
                     
-    with open( join(export_dir, dir_name + 'GtLabels'   + todays_date+ '.cfg' ), 'w' ) as f:
+    with open( join(export_dir, dir_name + 'GtLabels.cfg' ), 'w' ) as f:
         f.writelines("%s\n" % line for line in GtLabels)       
     
-    with open( join(export_dir, dir_name + 'Channels_t2grase'   + todays_date+ '.cfg' ), 'w' ) as f:
+    with open( join(export_dir, dir_name + 'Channels_t2grase.cfg' ), 'w' ) as f:
         f.writelines("%s\n" % line for line in Channels_t2grase)    
                     
-    with open( join(export_dir, dir_name + 'RoiMasks'   + todays_date+ '.cfg' ), 'w' ) as f:
+    with open( join(export_dir, dir_name + 'RoiMasks.cfg' ), 'w' ) as f:
         f.writelines("%s\n" % line for line in RoiMasks)                     
     
-    with open( join(export_dir, dir_name + 'Channels_t2tse'   + todays_date+ '.cfg' ), 'w' ) as f:
+    with open( join(export_dir, dir_name + 'Channels_t2tse.cfg' ), 'w' ) as f:
         f.writelines("%s\n" % line for line in Channels_t2tse)                     
     
-    with open( join(export_dir, dir_name + 'NamesOfPredictions'   + todays_date+ '.cfg' ), 'w' ) as f:
+    with open( join(export_dir, dir_name + 'NamesOfPredictions.cfg' ), 'w' ) as f:
         f.writelines("%s\n" % line for line in NamesOfPredictions)                     
                     
-    with open( join(export_dir, dir_name + 'Channels_adc'   + todays_date+ '.cfg' ), 'w' ) as f:
+    with open( join(export_dir, dir_name + 'Channels_adc.cfg' ), 'w' ) as f:
         f.writelines("%s\n" % line for line in Channels_adc)                     
     
     
@@ -81,12 +81,12 @@ def create_prostate_cfgs(main_dir, export_dir, search_term  ):
 
 # # training 
 # create_prostate_cfgs(main_dir = '/home/lenzmi/deepmedic/prostate_risk/data/withoutPIRADS/res_to_T2/train', 
-#                      export_dir = '/home/lenzmi/deepmedic/prostate_risk/configFiles/deepMedic/noP_T2TSE+ADC_T2_03032021/train', 
-#                      search_term  = '03032021')
+#                       export_dir = '/home/lenzmi/deepmedic/prostate_risk/configFiles/deepMedic/noP_T2TSE+ADC_T2_03032021/train', 
+#                       search_term  = '03032021')
 
 # # validation
 # create_prostate_cfgs(main_dir = '/home/lenzmi/deepmedic/prostate_risk/data/withoutPIRADS/res_to_T2/train/validation', 
-#                      export_dir = '/home/lenzmi/deepmedic/prostate_risk/configFiles/deepMedic/noP_T2TSE+ADC_T2_03032021/train/validation', 
-#                      search_term  = '03032021')
+#                       export_dir = '/home/lenzmi/deepmedic/prostate_risk/configFiles/deepMedic/noP_T2TSE+ADC_T2_03032021/train/validation', 
+#                       search_term  = '03032021')
 
 
