52165ee2df74e270e6b5b67ae5acecfeb16ac7d3
youpele52 <43122220+youpele52@users.noreply.github.com>
2021-03-03 13:44:18 +0100

    Add files via upload

--- resampling_prostate_03032021.py
+++ resampling_prostate_03032021.py
@@ -14,43 +14,45 @@ from resampling_prostate_main import resample
 from get_info import get_info
 join = os.path.join
 
+#  Resampling the dataset.. you have to change the main dir to test, validation and then to test
 
 todays_date = datetime.now().strftime('%d%m20%y')
-main_dir = '/home/lenzmi/deepmedic/prostate_risk/data/withoutPIRADS/res_to_T2/test'
+main_dir = '/home/lenzmi/deepmedic/prostate_risk/data/withoutPIRADS/res_to_T2/train'
 
 for patient in os.listdir(main_dir):
-    patient_dir = join(main_dir, patient)
-    
-    for content in os.listdir(patient_dir):
-        if 'ROI' in content:
-            patient_ROI = join(patient_dir, content)
-            out_patient_ROI = patient_ROI.split('.nii')[0]  +'_' + todays_date + ".nii.gz"
-            # print(patient_ROI,  out_patient_ROI )
-            resample(image_path = patient_ROI, 
-                     out_image_path = out_patient_ROI, 
-                     out_spacing=[1, 1, 1],
-                     is_label=True)
-            
-        elif 'Gtlabels' in content:
-            patient_GT = join(patient_dir, content)
-            out_patient_GT =  patient_GT.split('.nii')[0]  +'_' + todays_date + ".nii.gz"
-            # print(patient_GT,out_patient_GT )
-            resample(image_path = patient_GT , 
-                     out_image_path = out_patient_GT, 
-                     out_spacing=[1, 1, 1],
-                     is_label=True)
-        else:
-            patient_Image = join(patient_dir, content)
-            out_patient_Image = patient_Image.split('.nii')[0]  +'_' + todays_date + ".nii.gz"
-            resample(image_path  = patient_Image, 
-                     out_image_path = out_patient_Image, 
-                     out_spacing=[1, 1, 1],
-                     is_label=False)
-            
-            
+    if patient[0] == 'P':
+        # print(patient)
+        patient_dir = join(main_dir, patient)
         
-        
-        
+        for content in os.listdir(patient_dir):
+            if 'ROI' in content:
+                patient_ROI = join(patient_dir, content)
+                out_patient_ROI = patient_ROI.split('.nii')[0]  +'_' + todays_date + ".nii.gz"
+                # print(patient_ROI,  out_patient_ROI )
+                resample(image_path = patient_ROI, 
+                         out_image_path = out_patient_ROI, 
+                         out_spacing=[1, 1, 1],
+                         is_label=True)
+                
+            elif 'Gtlabels' in content:
+                patient_GT = join(patient_dir, content)
+                out_patient_GT =  patient_GT.split('.nii')[0]  +'_' + todays_date + ".nii.gz"
+                # print(patient_GT,out_patient_GT )
+                resample(image_path = patient_GT , 
+                         out_image_path = out_patient_GT, 
+                         out_spacing=[1, 1, 1],
+                         is_label=True)
+            else:
+                patient_Image = join(patient_dir, content)
+                out_patient_Image = patient_Image.split('.nii')[0]  +'_' + todays_date + ".nii.gz"
+                resample(image_path  = patient_Image, 
+                         out_image_path = out_patient_Image, 
+                         out_spacing=[1, 1, 1],
+                         is_label=False)
+            
+            
+
+
         
         
         
