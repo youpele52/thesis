d488c6e52790fa0e045120f5c7482faca46db1c6
Youpele Michael <mjayjesus@gmail.com>
2021-03-23 15:01:12 +0100

    Added docstring

--- resample_spacing.py
+++ resample_spacing.py
@@ -11,7 +11,29 @@ import SimpleITK as sitk
 
 def resample(image_path, out_image_path, segmentation_or_image,
                  out_spacing=[0.75, 0.75, 1], is_label=False):
+    # NOTE: if the script does not work, remove the entire docstring below.
+    '''
+    Resamples a given image.
+
+    Parameters
+    ----------
+    image_path : str
+        Path to the image.
+    out_image_path: str
+        Path to folder in which the resampled image will be saved.
+    segmentation_or_image: str
+        Used for naming of the output file.
+    out_spacing: list, Default is [0.75, 0.75, 1]
+        List containing three numbers that will be used to resample the image in x, y and z axis.
+    is_label: Boolean, Default is False.
+        If the image is a label, sitk.sitkNearestNeighbor is used to interpolate the image, else sitk.sitkBSpline is used.
     
+    Returns
+    -------
+    None.
+
+    '''
+ 
     itk_image = sitk.ReadImage(image_path)
     
     # Resample images to 2mm spacing with SimpleITK
@@ -41,8 +63,6 @@ def resample(image_path, out_image_path, segmentation_or_image,
     output_image = out_image_path +"/resampled_" +segmentation_or_image + ".nii"
     
     sitk.WriteImage(image, output_image)
-
-
 
 
 
--- resampling_prostate_main.py
+++ resampling_prostate_main.py
@@ -11,6 +11,26 @@ import SimpleITK as sitk
 
 def resample(image_path, out_image_path,
                  out_spacing=[0.75, 0.75, 1], is_label=False):
+    # NOTE: if the script does not work, remove the entire docstring below.
+    '''
+    Resamples a given image.
+
+    Parameters
+    ----------
+    image_path : str
+        Path to the image.
+    out_image_path: str
+        Path to folder in which the resampled image will be saved.
+    out_spacing: list, Default is [0.75, 0.75, 1]
+        List containing three numbers that will be used to resample the image in x, y and z axis.
+    is_label: Boolean, Default is False.
+        If the image is a label, sitk.sitkNearestNeighbor is used to interpolate the image, else sitk.sitkBSpline is used.
+    
+    Returns
+    -------
+    None.
+
+    '''
     
     itk_image = sitk.ReadImage(image_path)
     
